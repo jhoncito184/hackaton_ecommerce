5b456f47a40bad407e1da2e2179f8e7d76f0faba
John Olano <john_ol184@hotmail.com>
2021-07-31 16:43:18 -0500

    fix migrate

--- products_detail/urls.py
+++ products_detail/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProductDetailListAPIView, ProductDetailRetrieveAPIView
 
 urlpatterns = [
-    path('', ProductDetailListAPIView.as_view(), name='products_detail'),
-    path('<int:id>', ProductDetailRetrieveAPIView.as_view(), name='products_detail')
+    path('', ProductDetailListAPIView.as_view(), name='product_detail'),
+    path('<int:id>', ProductDetailRetrieveAPIView.as_view(), name='product_detail')
 ]
