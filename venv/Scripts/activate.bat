d8d3b2e9723293ac59b120e61184e6184aaaea9b
John Olano <john_ol184@hotmail.com>
2021-08-01 19:02:43 -0500

    fix schedule

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\hackaton_ecommerce\venv
+set VIRTUAL_ENV=D:\JHOM\idat\repos\hackathon\hackaton_ecommerce\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!d:\hackaton_ecommerce\venv\scripts\python.exe
+#!d:\jhom\idat\repos\hackathon\hackaton_ecommerce\venv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
