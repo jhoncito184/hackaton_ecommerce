bd83802a2902c76d7318c87b231b0a48c7373630
John Olano <john_ol184@hotmail.com>
2021-08-03 21:37:23 -0500

    add active product

--- schedule/models.py
+++ schedule/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Schedule(models.Model):
-    title = models.TextField()
     days = models.TextField()
     hours = models.TextField()
+    title = models.TextField(null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
