4b08cfdaf5ef01dec80846563665b7d01d3980a9
John Olano <john_ol184@hotmail.com>
2021-08-03 21:31:22 -0500

    add active product

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.TextField()
+    is_active = models.BooleanField(default=True)
     # image = models.ImageField(upload_to='products', max_length=255, blank=False, null=False, default='-' x)
 
     created_at = models.DateTimeField(auto_now_add=True)
