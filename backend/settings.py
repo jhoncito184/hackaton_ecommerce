c326b5622e08c7658716d1530f5774d66441f6ba
John Olano <john_ol184@hotmail.com>
2021-08-05 21:50:06 -0500

    add cors

--- backend/settings.py
+++ backend/settings.py
@@ -217,6 +217,33 @@ CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:9000'
 ]
 
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
