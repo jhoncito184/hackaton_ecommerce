7dc7b8f40f46b1b93dfb4586f1507d66f475844e
John Olano <john_ol184@hotmail.com>
2021-08-07 00:36:40 -0500

    add null coupon

--- coupon/models.py
+++ coupon/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Coupon(models.Model):
     email = models.TextField()
-    code = models.TextField(unique=True)
+    code = models.TextField(null=True)
     is_active = models.BooleanField(default=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
